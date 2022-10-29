579ea930ba6addeee79747f2e7550aeea9376a06
Sonali Sajwan <sonali.sajwan@swift-nav.com>
2022-10-29 12:35:45 +0530

    sum

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int x, int y)
+{
+    return x+y;
+}
+
+int main()
+{
+    cout<<"Hello Jenkins!"<<endl;
+
+    cout<<add(5,6)<<endl;
+    return 0;
+}
