eba6b60f261693cca6200775fe2570adc3d16400
Sonali Sajwan <sonali.sajwan@swift-nav.com>
2022-10-29 14:53:34 +0530

    gtest

---
+++ try g test/CMakeFiles/executeTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/executeTests.dir/sqrt_test.cpp.o"
+  "CMakeFiles/executeTests.dir/sqrt_test.cpp.o.d"
+  "executeTests"
+  "executeTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/executeTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
